5c09e8b24290dc28507bfd5d4a8308d7bab21f5e
isama22 <emarti17@artic.edu>
2021-02-17 23:49:51 -0600

    initial set up for components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './Home.js'
+import Upload from './Upload.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{background: 'grey', width: '100%', height: '100%', position: 'fixed'}}>
+      <h5>app</h5>
+      <Home/>
+      <Upload/>
     </div>
   );
 }
